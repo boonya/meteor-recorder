01ac796f75359b053004f58bc58917299b9de1ff
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2022-12-26 13:31:17 +0100

    Self hosted font

--- imports/ui/theme/index.js
+++ imports/ui/theme/index.js
@@ -2,6 +2,8 @@ import createComponents from './components';
 import createPalette from './palette';
 import createTypography from './typography';
 import {createTheme} from '@mui/material';
+// eslint-disable-next-line import/no-unassigned-import
+import '@fontsource/pangolin/index.css';
 
 export default (...args) => createTheme({
 	palette: createPalette(...args),
